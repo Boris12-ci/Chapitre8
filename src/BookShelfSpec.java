223a4ba6de2747b681781f2993a295c64a4a60d2
INAGO BORIS <Borisinago10@gmail.com>
2026-05-20 15:57:14 +0200

    test4

--- src/BookShelfSpec.java
+++ src/BookShelfSpec.java
@@ -1,4 +1,4 @@
-﻿package bookstoread;
+package bookstoread;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail; // Import manquant ajouté
 
 public class BookShelfSpec {
 
@@ -31,5 +32,18 @@ public class BookShelfSpec {
         shelf.add("Effective Java", "Code Complete", "The Mythical Man-Month");
         List<String> books = shelf.arrange(Comparator.<String>naturalOrder().reversed());
         assertEquals(Arrays.asList("The Mythical Man-Month", "Effective Java", "Code Complete"), books, "Books in bookshelf are in insertion order");
+    }
+
+    @Test
+    void booksReturnedFromBookShelfIsImmutableForClient() {
+        BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java", "Code Complete");
+        List<String> books = shelf.books();
+        try {
+            books.add("The Mythical Man-Month");
+            fail("Should not be able to add book to books");
+        } catch (Exception e) {
+            assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException.");
+        }
     }
 }
